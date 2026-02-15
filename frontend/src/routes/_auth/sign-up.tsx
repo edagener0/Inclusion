f90ac8c8f459ccad44e73ba3bdce1f035c530add
Vadym Lemnaru <vadlemnaru99@gmail.com>
2026-02-15 11:16:35 +0000

    chore(frontend/router): remove temporary development code

--- frontend/src/routes/_auth/sign-up.tsx
+++ frontend/src/routes/_auth/sign-up.tsx
@@ -2,12 +2,6 @@ import { createFileRoute } from '@tanstack/react-router';
 
 import { CenterSpinner } from '@/shared/ui/spinner';
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 export const Route = createFileRoute('/_auth/sign-up')({
-  pendingMs: 0,
-  pendingMinMs: 500,
-  loader: async () => {
-    await sleep(2000);
-  },
   pendingComponent: () => <CenterSpinner />,
 });
