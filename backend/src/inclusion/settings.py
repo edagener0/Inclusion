857516f2ce693c33f36799f98eb40e796d910ed5
edagener <edagener0@users.noreply.github.com>
2026-02-18 09:45:58 +0000

    Adicionou-se paginação default da API. Adicionou-se filtros para a ListView dos users.

--- backend/src/inclusion/settings.py
+++ backend/src/inclusion/settings.py
@@ -56,6 +56,11 @@ REST_FRAMEWORK = {
         'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+    ],
 }
 
 # Application definition
@@ -68,6 +73,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'drf_spectacular',
+    'django_filters',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'rest_framework',
--- backend/src/users/views.py
+++ backend/src/users/views.py
@@ -8,16 +8,18 @@ from .serializers import (
     UserDetailUpdateSerializer
 )
 from rest_framework.permissions import IsAuthenticated
+from .filters import UserFilter
+
 User = get_user_model()
 
 class UserListView(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserListSerializer
     permission_classes = [IsAuthenticated]
+    filterset_class = UserFilter
 
 class UserDetailUpdateView(RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserDetailUpdateSerializer
     permission_classes = [IsAuthenticated]
     http_method_names = ["get", "patch", "options", "head"]
-
