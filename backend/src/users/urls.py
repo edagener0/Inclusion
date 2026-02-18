60bd15723aa7dd9b94d7135b4fbcbd8434fd8945
edagener <edagener0@users.noreply.github.com>
2026-02-18 14:42:23 +0000

    Adicionou-se uma View para procurar utilizadores pelo nome completo e username

--- backend/src/users/urls.py
+++ backend/src/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
     UserListView,
-    UserDetailUpdateView
+    UserDetailUpdateView,
+    UserSearchView
 )
 
 urlpatterns = [
     path("list/", UserListView.as_view(), name="user-list"),
     path("<int:pk>/", UserDetailUpdateView.as_view(), name="user-detail-update"),
+    path("search/<str:name>/", UserSearchView.as_view(), name="user-search-list"),
 ]
--- backend/src/users/views.py
+++ backend/src/users/views.py
@@ -1,7 +1,8 @@
 from rest_framework.generics import (
     ListAPIView,
-    RetrieveUpdateAPIView
+    RetrieveUpdateAPIView,
 )
+from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
 from .serializers import (
     UserListSerializer,
@@ -9,7 +10,16 @@ from .serializers import (
 )
 from rest_framework.permissions import IsAuthenticated
 from .filters import UserFilter
-
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from drf_spectacular.utils import (
+    extend_schema, 
+    OpenApiParameter
+)
+from drf_spectacular.types import OpenApiTypes
+from rest_framework import status
 User = get_user_model()
 
 class UserListView(ListAPIView):
@@ -23,3 +33,41 @@ class UserDetailUpdateView(RetrieveUpdateAPIView):
     serializer_class = UserDetailUpdateSerializer
     permission_classes = [IsAuthenticated]
     http_method_names = ["get", "patch", "options", "head"]
+
+class UserSearchView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserListSerializer
+    pagination_class = PageNumberPagination
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="page",
+                type=OpenApiTypes.INT,
+                location=OpenApiParameter.QUERY,
+                description="The page number for the users.",
+            )
+        ],
+        responses={status.HTTP_200_OK: UserListSerializer(many=True)}
+    )
+    def get(self, request, name):
+        users = User.objects.annotate(
+            full_name = Concat(
+                "first_name",
+                Value(" "),
+                "last_name"
+            )
+        ).filter(
+            Q(username__icontains = name) |
+            Q(first_name__icontains = name) |
+            Q(last_name__icontains = name) |
+            Q(full_name__icontains = name)
+        )
+        
+        paginator = self.pagination_class()
+        page = paginator.paginate_queryset(
+            users, request, view=self
+        )
+
+        serializer = self.serializer_class(page, many=True)
+        return paginator.get_paginated_response(serializer.data)
