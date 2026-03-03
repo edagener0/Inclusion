1f24e982fe7dbd17e27875e2f6311d83b80acb34
GuilhermeCarmo6 <gui.t.carmo@gmail.com>
2026-03-03 15:40:45 +0000

    Fix CI errors

--- frontend/src/widgets/header/ui/Header.tsx
+++ frontend/src/widgets/header/ui/Header.tsx
@@ -3,8 +3,8 @@ import { useState } from 'react';
 import { Link } from '@tanstack/react-router';
 import { Menu, Search } from 'lucide-react';
 
-
 import { ModeToggle } from '@/features/mode-toggle';
+
 import { SidebarNav } from '@/features/sidebar';
 import { UserDropDownMenu } from '@/features/user/user-menu';
 import { AppLogo } from '@/shared/assets/icons/AppIcon';
