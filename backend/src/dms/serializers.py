088d24eec26815b6e3bbab5458f8e619d8060f4c
Monstercreft <gabivaz96@gmail.com>
2026-03-11 13:49:32 +0000

    feat(messages): Continuaçao da implementaçao  serializers e e views nas DMs

--- backend/src/dms/serializers.py
+++ backend/src/dms/serializers.py
@@ -1,22 +1,23 @@
 from rest_framework import serializers
 from .models import DM
 
+# 
 class DMSerializer(serializers.ModelSerializer):
     class Meta:
         model = DM
         fields = ['id', 'content', 'sender', 'receiver', 'created_at']
         read_only_fields = ['id', 'sender', 'created_at']
 
+# Serializer para criação de uma mensagem DM, onde o remetente é o usuário autenticado
 class DMCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = DM
         fields = ['content', 'receiver']
 
+# Devolver as conversas de todos os DMs do usuário com os diferentes destinatários, mostrando o username do remetente e destinatário
 class DMListSerializer(serializers.ModelSerializer):
-    sender_username = serializers.CharField(source='sender.username', read_only=True)
     receiver_username = serializers.CharField(source='receiver.username', read_only=True)
-
     class Meta:
         model = DM
-        fields = ['id', 'content', 'sender_username', 'receiver_username', 'created_at']
-        read_only_fields = ['id', 'sender_username', 'receiver_username', 'created_at']
+        fields = ['id', 'sender', 'receiver', 'receiver_username', 'created_at']
+        read_only_fields = ['id', 'sender', 'created_at']
--- backend/src/dms/views.py
+++ backend/src/dms/views.py
@@ -10,15 +10,34 @@ from django.db.models import Q
 from .models import DM
 from .serializers import DMSerializer, DMCreateSerializer, DMListSerializer
 
+# DMListView: Listar apenas as DM que o usuario tem com outros recivers, mostrando o username do remetente e destinatário, e para criar uma nova mensagem DM, onde o remetente é o usuário autenticado
 class DMListView(ListCreateAPIView):
     serializer_class = DMListSerializer
     permission_classes = [IsAuthenticated]
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return DMCreateSerializer
+        return DMListSerializer
     
     def get_queryset(self):
         return DM.objects.filter(
             Q(sender=self.request.user) | Q(receiver=self.request.user) #union
         ).order_by("-created_at")
-    
+
+
+
+# DMListMessageView: Listar as mensagens dentro do DM do usuário autenticado com um determinado receiver, mostrando o username do remetente e destinatário, e para criar uma nova mensagem DM, onde o remetente é o usuário autenticado
+class DMListMessageView(ListCreateAPIView):
+    serializer_class = DMSerializer
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        receiver_id = self.kwargs.get("receiver_id")
+        return DM.objects.filter(
+            (Q(sender=self.request.user) & Q(receiver__id=receiver_id)) | 
+            (Q(sender__id=receiver_id) & Q(receiver=self.request.user))
+        ).order_by("-created_at")
+
+
 class DMRetrieveDestroyView(RetrieveDestroyAPIView):
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     queryset = DM.objects.all()
