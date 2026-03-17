65166e9cd2921cc93822c05f0cd5ac2ddad29d14
Vadym Lemnaru <vadlemnaru99@gmail.com>
2026-03-17 14:53:12 +0000

    feat(frontend): redirect from index to posts page

--- frontend/src/routes/_main/index.tsx
+++ frontend/src/routes/_main/index.tsx
@@ -1,7 +1,3 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
-export const Route = createFileRoute('/_main/')({ component: Index });
-
-function Index() {
-  return <div className="max-w-2xl mx-auto space-y-6">Info</div>;
-}
+export const Route = createFileRoute('/_main/')({ beforeLoad: () => redirect({ to: '/posts' }) });
