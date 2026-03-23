206ffecc301e1daf465d7684c7bc0226cdcd9501
Vadym Lemnaru <vadlemnaru99@gmail.com>
2026-03-23 17:37:10 +0000

    style(frontend): adjust layout size

--- frontend/src/widgets/header/ui/Header.tsx
+++ frontend/src/widgets/header/ui/Header.tsx
@@ -19,7 +19,7 @@ export function Header({ sideBarSlot, userMenuSlot }: Props) {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
-      <div className="container mx-auto max-w-6xl flex h-16 items-center justify-between px-4">
+      <div className="container mx-auto max-w-7xl flex h-16 items-center justify-center gap-x-8 px-4">
         <div className="flex md:w-50 shrink-0 items-center gap-2">
           <div className="md:hidden">
             <Sheet open={isSidebarOpen} onOpenChange={setIsSidebarOpen}>
@@ -43,20 +43,22 @@ export function Header({ sideBarSlot, userMenuSlot }: Props) {
           </Link>
         </div>
 
-        <div className="flex-1 flex items-center px-2 md:px-6">
-          {/* TODO: make it dynamic */}
-          <h1 className="text-sm md:text-lg font-bold truncate">News</h1>
-        </div>
-
-        <div className="flex items-center gap-2 md:gap-4 shrink-0">
-          <div className="relative hidden sm:block w-40 md:w-50">
-            <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
-            <Input
-              placeholder="Search..."
-              className="pl-9 rounded-full h-9 bg-muted/50 border-none focus-visible:ring-1"
-            />
+        <div className="flex w-full max-w-2xl items-center justify-between">
+          {/* Title */}
+          <div className="flex-1 flex items-center md:px-0">
+            <h1 className="text-sm md:text-lg font-bold truncate">News</h1>
           </div>
-          {userMenuSlot}
+
+          <div className="flex items-center gap-2 md:gap-4 shrink-0">
+            <div className="relative hidden sm:block w-40 md:w-50">
+              <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
+              <Input
+                placeholder="Search..."
+                className="pl-9 rounded-full h-9 bg-muted/50 border-none focus-visible:ring-1"
+              />
+            </div>
+            {userMenuSlot}
+          </div>
         </div>
       </div>
     </header>
