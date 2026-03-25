cdc9c00ead2dd94c02c9985bc5042dd91506b66e
Vadym Lemnaru <vadlemnaru99@gmail.com>
2026-03-25 12:33:37 +0000

    fix(frontend/api): change schema user avatar field from httpUrl to url

--- frontend/src/shared/api/schemas/user-preview.schema.ts
+++ frontend/src/shared/api/schemas/user-preview.schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const UserPreviewSchema = z.object({
   id: z.int().positive(),
   username: z.string(),
-  avatar: z.httpUrl(),
+  avatar: z.url(),
 });
 
 export type UserPreview = z.infer<typeof UserPreviewSchema>;
