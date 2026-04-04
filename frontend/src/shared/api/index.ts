d7400b1cbcd48865c721fc119cbe8ca39aa76da4
jethroTLOU <60586776+jethroTLOU@users.noreply.github.com>
2026-04-04 19:31:34 +0100

    feat(frontend): add any infinite option query type

--- frontend/src/shared/api/index.ts
+++ frontend/src/shared/api/index.ts
@@ -1,4 +1,4 @@
 export { api } from './base';
 export { queryClient } from './query-client';
-export { type PaginatedResponse } from './types';
+export { type PaginatedResponse, type AnyInfiniteOptions } from './types';
 export { UserPreviewSchema, type UserPreview } from './schemas';
--- frontend/src/shared/api/types.ts
+++ frontend/src/shared/api/types.ts
@@ -1,6 +1,19 @@
+import type { InfiniteData, UndefinedInitialDataInfiniteOptions } from '@tanstack/react-query';
+
 export type PaginatedResponse<T> = {
   count: number;
   next: string | null;
   previous: string | null;
   results: T[];
 };
+
+export type AnyInfiniteOptions<TData, TError = Error> = UndefinedInitialDataInfiniteOptions<
+  TData,
+  TError,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  InfiniteData<TData, any>,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  any,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  any
+>;
