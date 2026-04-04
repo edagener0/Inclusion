a53086851255163759b114a8d975066ba3687257
jethroTLOU <60586776+jethroTLOU@users.noreply.github.com>
2026-04-04 21:34:53 +0100

    refactor(frontend): remove unused exports, fix vulnerability

--- frontend/src/features/friend/frindship-manage/index.ts
+++ frontend/src/features/friend/frindship-manage/index.ts
@@ -1,6 +1,3 @@
-export { SendRequestButton } from './ui/SendRequestButton';
 export { AcceptRequestButton } from './ui/AcceptRequestButton';
 export { DeclineRequestButton } from './ui/DeclineRequestButton';
-export { CancelRequestButton } from './ui/CancelRequestButton';
-export { RemoveFriendButton } from './ui/RemoveFriendButton';
 export { FriendshipManageButton } from './ui/FrindshipManageButton';
