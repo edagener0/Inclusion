d253ad05877cf6462dc5ff3de0572b8513b03b5d
dretti <190618247+GuilhermeCarmo6@users.noreply.github.com>
2026-04-10 17:47:00 +0100

    fix: Fix CI build (again T_T)

--- frontend/src/entities/wordle/index.ts
+++ frontend/src/entities/wordle/index.ts
@@ -1,5 +1,6 @@
 export { parseDiff, type LetterStatus } from './lib/parse-diff';
-export { checkLenght } from './lib/check-length';
+export { checkLength } from './lib/word-validator';
 export * from './model/schema';
 export { wordleQueries } from './api/queries';
-export { getWordleWord, submitGuess, getWordleLeardboard } from './api/requests';
+export { getWordleWord, submitGuess, getWordleLeaderboard } from './api/requests';
+export { WORDLE_CONFIG, WORDLE_SYMBOLS } from './lib/constants';
