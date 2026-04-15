0a23ce5de1b2e56f2f50d16301aa284fdff6d4c5
edagener <edagener0@users.noreply.github.com>
2026-04-15 14:51:42 +0100

    feat(dockerfile): fez-se o dockerfile para deploy da aplicação.

--- backend/src/inclusion/settings.py
+++ backend/src/inclusion/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 AUTH_USER_MODEL = "authentication.User"
 
