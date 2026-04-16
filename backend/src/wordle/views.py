10b96d16359dd5360b0c332e3eb8ca69b2a56d9a
jethroTLOU <60586776+jethroTLOU@users.noreply.github.com>
2026-04-16 17:16:55 +0100

    feat(backend): add gameId on response object

--- backend/src/wordle/views.py
+++ backend/src/wordle/views.py
@@ -11,7 +11,7 @@ from .serializers import (
     WordleGuessSerializer,
     WordleGuessResponseSerializer,
     WordleGuessErrorSerializer,
-    WordleLeaderboardUserSerializer
+    WordleLeaderboardUserSerializer,
 )
 from .utils import find_diff_between_words, RIGHT_PLACE_SYMBOL
 from rest_framework.generics import ListAPIView
@@ -20,15 +20,13 @@ from .utils import update_user_wordle_streak
 
 User = get_user_model()
 
+
 class WordleWordView(APIView):
     permission_classes = [IsAuthenticated]
 
     @extend_schema(
-        responses={
-            200: WordSerializer,
-            404: WordleGuessErrorSerializer
-        },
-        description="Get today's Wordle metadata. Returns the word length and difficulty."
+        responses={200: WordSerializer, 404: WordleGuessErrorSerializer},
+        description="Get today's Wordle metadata. Returns the word length and difficulty.",
     )
     def get(self, request, *args, **kwargs):
         today = timezone.now().date()
@@ -36,11 +34,14 @@ class WordleWordView(APIView):
         wordle = get_object_or_404(Wordle, date=today)
 
         has_won = WordleResult.objects.filter(
-            wordle=wordle, 
-            user=self.request.user, 
-            status=WordleResult.StatusChoices.SUCCESS).exists()
-        
-        serializer = WordSerializer(wordle.word, context={"has_won": has_won})
+            wordle=wordle,
+            user=self.request.user,
+            status=WordleResult.StatusChoices.SUCCESS,
+        ).exists()
+
+        serializer = WordSerializer(
+            wordle.word, context={"has_won": has_won, "game_id": wordle.id}
+        )
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -55,35 +56,30 @@ class WordleGuessView(APIView):
             400: WordleGuessErrorSerializer,
             404: WordleGuessErrorSerializer,
         },
-        description="Submit your guess for today's Wordle. Returns whether the guess is correct, the number of guesses, or errors if invalid."
+        description="Submit your guess for today's Wordle. Returns whether the guess is correct, the number of guesses, or errors if invalid.",
     )
-
     def post(self, request, *args, **kwargs):
         serializer = WordleGuessSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         guessed_word = serializer.validated_data["word"]
-        
+
         today = timezone.now().date()
         wordle = get_object_or_404(Wordle, date=today)
-        
+
         if not guessed_word:
             return Response(
-                {
-                    "detail": "No guessed word."
-                },
-                status=status.HTTP_400_BAD_REQUEST
+                {"detail": "No guessed word."}, status=status.HTTP_400_BAD_REQUEST
             )
-        
+
         if len(guessed_word) != wordle.word.length:
             return Response(
-                {
-                    "detail": "Invalid guess length"
-                },
-                status=status.HTTP_400_BAD_REQUEST
+                {"detail": "Invalid guess length"}, status=status.HTTP_400_BAD_REQUEST
             )
-        
-        wordle_result, created = WordleResult.objects.get_or_create(wordle=wordle, user=self.request.user)
+
+        wordle_result, created = WordleResult.objects.get_or_create(
+            wordle=wordle, user=self.request.user
+        )
 
         if not created and wordle_result.status == WordleResult.StatusChoices.SUCCESS:
             return Response(
@@ -91,11 +87,11 @@ class WordleGuessView(APIView):
                     "detail": "You already guessed today's wordle!",
                     "correct": True,
                     "guesses": wordle_result.guesses,
-                    "diff": RIGHT_PLACE_SYMBOL * wordle.word.length
+                    "diff": RIGHT_PLACE_SYMBOL * wordle.word.length,
                 },
-                status=status.HTTP_200_OK
+                status=status.HTTP_200_OK,
             )
-        
+
         is_correct = guessed_word.lower() == wordle.word.text.lower()
         diff = find_diff_between_words(guessed_word.lower(), wordle.word.text.lower())
 
@@ -105,15 +101,16 @@ class WordleGuessView(APIView):
 
         wordle_result.guesses += 1 if not created else 0
         wordle_result.save()
-        
+
         return Response(
             {
-                "detail": "Your guess was " + ("correct" if is_correct else "incorrect"),
+                "detail": "Your guess was "
+                + ("correct" if is_correct else "incorrect"),
                 "correct": is_correct,
                 "guesses": wordle_result.guesses,
                 "diff": diff,
             },
-            status=status.HTTP_200_OK
+            status=status.HTTP_200_OK,
         )
 
 
@@ -121,4 +118,4 @@ class WordleLeaderboardView(ListAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = WordleLeaderboardUserSerializer
     queryset = User.objects.order_by("-max_wordle_streak")
-        
+
